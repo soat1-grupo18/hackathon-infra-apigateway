0f48b240771e286def2b3b80fadd46184f51bc81
Leandro Gonçalves de Oliveira <olegon.main+github@gmail.com>
2024-03-23 15:22:15 -0300

    chore: configures cognito

--- src/aws-cognito.tf
+++ src/aws-cognito.tf
@@ -2,6 +2,14 @@ resource "aws_cognito_user_pool" "this" {
   name = "fiap-hackathon-pool"
 }
 
+resource "aws_cognito_user_pool_client" "fiap_hackathon_api" {
+  name         = "fiap-hackathon-api"
+  user_pool_id = aws_cognito_user_pool.this.id
+  explicit_auth_flows = [
+    "USER_PASSWORD_AUTH"
+  ]
+}
+
 resource "aws_cognito_user" "olegon" {
   user_pool_id = aws_cognito_user_pool.this.id
   username     = "olegon"
